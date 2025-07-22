8b0db020201baaf1b409b8fd47a8b809079c5240
Nguyễn Mạnh Hùng <nguyenmanhhung20184@gmail.com>
2025-07-22 23:57:20 +0700

    update bullet effect and code

--- Assets/Scripts/GameItem/ExpBall.cs
+++ Assets/Scripts/GameItem/ExpBall.cs
@@ -1,19 +1,50 @@
-﻿
-using System;
+﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class ExpBall : MonoBehaviour, IInteractable
 {
     [SerializeField] private float exp;
     [SerializeField] private float speed;
+    [SerializeField] private float hoverHeight = 0.3f; 
+    [SerializeField] private float hoverDuration = 0.3f; 
 
     private PlayerMovement _playerTarget;
     private bool _isAttracting;
+    private bool _canAttract = false;
+
+    private void OnEnable()
+    {
+        _canAttract = false;
+        StartCoroutine(HoverEffect());
+    }
+
+    private IEnumerator HoverEffect()
+    {
+        Vector3 originalPos = transform.position;
+        Vector3 hoverPos = originalPos + Vector3.up * hoverHeight;
+
+        float timer = 0;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime / (hoverDuration / 2);
+            transform.position = Vector3.Lerp(originalPos, hoverPos, timer);
+            yield return null;
+        }
+
+        timer = 0;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime / (hoverDuration / 2);
+            transform.position = Vector3.Lerp(hoverPos, originalPos, timer);
+            yield return null;
+        }
+
+        _canAttract = true;
+    }
 
     private void Update()
     {
-        if (_isAttracting && _playerTarget)
+        if (_isAttracting && _playerTarget && _canAttract)
         {
             transform.position = Vector3.MoveTowards(transform.position, _playerTarget.transform.position, speed * Time.deltaTime);
 
@@ -21,13 +52,15 @@ public class ExpBall : MonoBehaviour, IInteractable
             {
                 _playerTarget.Exp(exp);
                 _isAttracting = false;
-                this.gameObject.SetActive(false);
+                gameObject.SetActive(false);
             }
         }
     }
+
     public void Interact(PlayerMovement player)
-    {   
+    {
         _playerTarget = player;
         _isAttracting = true;
     }
+    
 }
