557e7f8aced2aeca8bfea376bf94cdb8708d0687
Nguyễn Mạnh Hùng <nguyenmanhhung20184@gmail.com>
2025-08-03 18:28:58 +0700

    add boss

--- Assets/_MyGame/Code/Scripts/DamageIndicatorSystem/EnemyDetected.cs
+++ Assets/_MyGame/Code/Scripts/DamageIndicatorSystem/EnemyDetected.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class TestDetected : MonoBehaviour
+public class EnemyDetected : MonoBehaviour
 {
     [SerializeField] private EnemyBase enemy;
     private void OnEnable()
@@ -18,12 +18,12 @@ public class TestDetected : MonoBehaviour
         {
             if (!DI_System.Instance.CheckIfObjectInSight(this.transform))
             {
-                Debug.Log("Object not in sight");
+                //Debug.Log("Object not in sight");
                 DI_System.Instance.CreateIndicator(enemy);
             }
             else
             {
-                Debug.Log("Object is already in sight.");
+                //Debug.Log("Object is already in sight.");
                 DI_System.Instance.RemoveIndicator(enemy.indicatorID);
             }
             yield return new WaitForSeconds(1f);
---
+++ Assets/_MyGame/Code/Scripts/Enemy/Boss/State/EnemyMoveState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveState : EnemyState
+{
+    public EnemyMoveState(EnemyStateMachine stateMachine, EnemyBase enemy) : base(stateMachine, enemy)
+    {
+    }
+    
+}
